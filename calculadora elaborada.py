4c476726ed8c32631c4e418f8d1827ce7bce8796
reinadodopython <42078612+reinadodopython@users.noreply.github.com>
2018-08-03 22:03:08 -0300

    calculadora
    calculadora python

---
+++ calculadora elaborada.py
@@ -0,0 +1,24 @@
+n1=int(input("digite numero :"))
+n2=int(input("digite numero :"))
+oper=input("digite soma = +,subtração = -,multiplicação = *,divisao = /:")
+
+#soma
+if (oper=="+"):
+ print("resultado = ",n1+n2)
+
+#subtração
+if (oper=="-"):
+  print("resultado = ",n1-n2)
+
+#multiplicação
+if (oper=="*"):
+  print("resultado = ",n1*n2)
+
+#divisão
+if (oper=="/"):
+  print("resultado = ",n1/n2)
+
+
+
+
+
